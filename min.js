d33d079b2293078390f60ac3f9b8f9e1f63c5ae2
prachi singh <prachisingh7921@gmail.com>
2023-06-30 11:25:30 +0530

    first commit

---
+++ min.js
@@ -0,0 +1,14 @@
+// 4. Write a JavaScript program to check if a number is even or odd.
+
+let numb1 = 3;
+let numb2 = 8;
+let numb3 = (numb1, numb2);
+function number(numb1, numb2) {
+    if (numb1 % 2 == 0){
+        return Even;
+    }
+    else{
+        return Odd;
+    }
+}
+document.write(numb3);
